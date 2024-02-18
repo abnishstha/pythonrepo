8e324c190a918a21b26229b724b08d90f5dfe590
abnishstha <abnishstha@gmail.com>
2024-02-18 12:15:49 +1000

    python

--- webapp.py
+++ webapp.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
  status = '200 OK'
- output = b'Hellow world this is py powered web app!!'
+ output = b'Hellow world this is py powered web app via CICD!!'
  response_headers = [('Content-type', 'text/plain'),
  ('Content-Length', str(len(output)))]
  start_response(status, response_headers)
